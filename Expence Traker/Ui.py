f38b8dece451c64c640da2b2bbb7b3f2b2ba58d7
Sophia Peschong <peschongsophia@gmail.com>
2026-04-08 13:07:33 -0500

    Remove unwanted files and folders

--- Expence Traker/Ui.py
+++
@@ -1,8 +0,0 @@
-def show_menu():
-    print("/nMenu: ") #/n creates an empy space from the default python code
-    print("1. add expense")
-    print("2. print expenses")
-    print("3. show the summary")
-    print("4. exit")
-
-# UI only is the menu display
